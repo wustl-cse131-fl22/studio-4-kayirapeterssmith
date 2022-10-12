7c3cc5866b2552d4149d6e34bbfcda80815148f4
lego1 <lego1@DESKTOP-8QVCDFC.dhcp.wustl.edu>
2022-10-12 15:34:21 -0500

    Complete

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,60 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner in = new Scanner(f); // making Scanner with a File
+		String shapeType = in.next();
+		if (shapeType.equals("rectangle")) {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			Color shapeColor = new Color(redComponent, greenComponent, blueComponent);
+			StdDraw.setPenColor(shapeColor);
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			} else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		} else if (shapeType.equals("ellipse")) {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			Color shapeColor = new Color(redComponent, greenComponent, blueComponent);
+			StdDraw.setPenColor(shapeColor);
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			} else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		} else if (shapeType.equals("triangle")) {
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			Color shapeColor = new Color(redComponent, greenComponent, blueComponent);
+			StdDraw.setPenColor(shapeColor);
+			double[] x = { x1, x2, x3 };
+			double[] y = { y1, y2, y3 };
+			if (isFilled == true) {
+				StdDraw.filledPolygon(x, y);
+			} else {
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
